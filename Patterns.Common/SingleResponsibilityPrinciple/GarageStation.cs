321734da176c1ec75ec578d14a5df1a57a87e465
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2020-07-09 16:24:46 -0500

    Vehicle

---
+++ Patterns.Common/SingleResponsibilityPrinciple/GarageStation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patterns.Common.SingleResponsibilityPrinciple
+{
+    public class GarageStation
+    {
+        public void DoOpenGate()
+        {
+            //Open the gate functinality
+        }
+
+        public void PerformService(Vehicle vehicle)
+        {
+            //Check if garage is opened
+            //finish the vehicle service
+        }
+
+        public void DoCloseGate()
+        {
+            //Close the gate functinality
+        }
+    }
+}

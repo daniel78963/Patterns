1d9b065149949947808b3c5903817641b1f1e21b
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2020-07-22 11:45:29 -0500

    Prototype

---
+++ Patterns.Common/1_CreationalDesignPatterns/4_1_Prototype/1/IdInfo.cs
@@ -0,0 +1,13 @@
+﻿ 
+namespace Patterns.Common._1_CreationalDesignPatterns._4_1_Prototype._1
+{
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+}
---
+++ Patterns.Common/1_CreationalDesignPatterns/4_1_Prototype/1/Person.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Patterns.Common._1_CreationalDesignPatterns._4_1_Prototype._1
+{
+    using System;
+
+    public class Person
+    {
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public IdInfo IdInfo;
+
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person clone = (Person)this.MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            clone.Name = String.Copy(Name);
+            return clone;
+        }
+    }
+}

16d82dd495e033b7f07f5e849c217417c058a0db
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2021-07-27 06:34:11 -0500

    builder 2021

---
+++ Patterns.Common/1_CreationalDesignPatterns/1_3_Builder/2/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Patterns.Common._1_CreationalDesignPatterns._1_3_Builder._2
+{
+    public class User
+    {
+        private string name;
+        private string username;
+        private long age;
+        private string city;
+
+        public User(string name, string username, long age, string city)
+        {
+            this.name = name;
+            this.username = username;
+            this.age = age;
+            this.city = city;
+        }
+    }
+}
